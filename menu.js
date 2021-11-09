350558f6a5938f575191f940563ba26d6d0fcff3
Munkh-Orgill <mnkhrglkd@gmail.com>
2021-11-09 16:09:14 +0800

    fix navbar

---
+++ menu.js
@@ -0,0 +1,23 @@
+const btnHam = document.querySelector('.menu-btn');
+
+const btnClose = document.querySelector('.close-btn');
+
+const navBar = document.getElementById('nav')
+
+btnHam.addEventListener('click', function(){
+    if(btnHam.className !== "")
+    {
+        btnHam.style.display = "none";
+        btnClose.style.display = "block";
+        navBar.classList.add("show-nav");
+    } 
+})
+
+btnClose.addEventListener('click', function(){
+    if(btnHam.className !== "")
+    {
+        this.style.display = "none";
+        btnHam.style.display = "block";
+        navBar.classList.remove("show-nav");
+    }
+})
---
+++ module/Medee.js
@@ -0,0 +1,15 @@
+export default class Medee{
+    constructor(garchig, onSar, likeToo, setgegdelToo, zurag){
+        this.title = garchig;
+        this.date = onSar;
+        this.like = likeToo;
+        this.comment = setgegdelToo;
+        this.image = "";
+    }
+    
+
+    GetHtml(){
+         
+        return html ``
+    }
+}
